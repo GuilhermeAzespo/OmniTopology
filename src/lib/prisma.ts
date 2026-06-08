31a2b0c3ba2db4581b83e4825e9f602a06781513
Guilherme Azespo <guilherme@azespo.com>
2026-06-08 11:28:55 -0300

    fix: use datasources instead of adapter for PrismaClient connection string

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -8,8 +8,10 @@ export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
     log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
-    adapter: {
-      url: process.env.DATABASE_URL as string,
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL as string,
+      },
     },
   });
 
